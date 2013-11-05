708e8b0e5deb7262bfa17b9b494d1661888b4276
Nathan <nate.good@showclix.com>
2013-11-05 14:08:06 -0500

    forgot to concatenate update + final
    this was causing simplecrypt to fail on larger bodies of text.  fixed and
    beefed up the tests to cover more cases

--- tests/test.js
+++ tests/test.js
@@ -3,20 +3,30 @@ var simplecrypt = require('../lib/simplecrypt');
 
 describe('simplecrypt', function() {
     var sc = simplecrypt();
-    var message = 'my secret';
+    var messages = [
+        'mysecret',
+        'my secret',
+        'a phrase with many spaces',
+        'apasswordwith123456',
+        'sp3c!@lcH4R',
+        'A long phrase with many words and things of that nature.',
+        Array(1000).join('abcdef1234 ')
+    ];
 
-    describe('#encrypt', function() {
-        it('should encrypt a string to a hex string', function() {
-            var digest = sc.encrypt(message);
-            assert.equal(typeof digest, 'string');
-            assert.notEqual(digest.match(/^[a-f0-9]+$/i), null);
+    messages.forEach(function(message) {
+        describe('encrypt', function() {
+            it('should encrypt a string to a hex string', function() {
+                var digest = sc.encrypt(message);
+                assert.equal(typeof digest, 'string');
+                assert.notEqual(digest.match(/^[a-f0-9]+$/i), null);
+            });
         });
-    });
 
-    describe('#decrypt', function() {
-        it('should decrypt a string to its original message', function() {
-            var digest = sc.encrypt(message);
-            assert.equal(sc.decrypt(digest), message);
+        describe('decrypt', function() {
+            it('should decrypt a string to its original message', function() {
+                var digest = sc.encrypt(message);
+                assert.equal(sc.decrypt(digest), message);
+            });
         });
     });
 });
