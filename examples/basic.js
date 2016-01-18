2afe566b06c67c0ae805582bc61ce61db85e0fe6
Lit Phansiri <phansiri@uw.edu>
2016-01-18 10:37:01 -0800

    commented on basic.js utilizing jsdoc, provided 3 more examples, and added .gitignore

--- examples/basic.js
+++ examples/basic.js
@@ -1,9 +1,44 @@
+/**
+ * Imports module for usage
+ * @type {*|exports|module.exports}
+ */
 var simplecrypt = require("../lib/simplecrypt");
 
+/**
+ * Simple example of encryption/decryption without a parameter
+ */
 var sc = simplecrypt();
-
 var digest = sc.encrypt("my secret");
 console.log(digest); // "66cea6eb1c18b8862485cf0604fa6062"
-
 var message = sc.decrypt(digest);
 console.log(message); // "my secret"
+
+/**
+ * Example of using sha256 as encryption strength
+ * @type {{encrypt, decrypt, password, salt}|{encrypt: encrypt, decrypt: decrypt, password: password, salt: salt}|*}
+ */
+var sha = simplecrypt('sha256');
+var encryptText = sha.encrypt("This is my secret text using sha256 as my strength of encryption");
+console.log(encryptText);
+var decryptedText = sha.decrypt(encryptText);
+console.log(decryptedText);
+
+/**
+ * Example of using rsa-sha256 as encryption strength
+ * @type {{encrypt, decrypt, password, salt}|{encrypt: encrypt, decrypt: decrypt, password: password, salt: salt}|*}
+ */
+var sha = simplecrypt('rsa-sha256');
+var encryptText = sha.encrypt("This is my secret text using sha256 as my strength of encryption");
+console.log(encryptText);
+var decryptedText = sha.decrypt(encryptText);
+console.log(decryptedText);
+
+/**
+ * Example of using sha512 as encryption strength
+ * @type {{encrypt, decrypt, password, salt}|{encrypt: encrypt, decrypt: decrypt, password: password, salt: salt}|*}
+ */
+var sha = simplecrypt('sha512');
+var encryptText = sha.encrypt("This is my secret text using sha512 as my strength of encryption");
+console.log(encryptText);
+var decryptedText = sha.decrypt(encryptText);
+console.log(decryptedText);
